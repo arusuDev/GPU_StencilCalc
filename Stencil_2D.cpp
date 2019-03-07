e502fb5cf152c5586895b3558fd6ec7c864ae3d8
Arusu <arusu.kanzaki@gmail.com>
2019-03-07 18:46:38 +0900

    2D CPU+1GPU

--- Stencil_2D.cpp
+++ Stencil_2D.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-#include <stdio.h>
-//#include <chrono> //C++11
+#include <chrono> //C++11
 #include <cstring>
 #include <cstdlib>
 #include <utility>	//C++11
 
 #define BLOCK 32
 
-#define X 32
-#define Y 32
+#define X 1024
+#define Y 1024
 
 #define ELEM (size_t)(X*Y)
 #define STEP 128
@@ -22,26 +21,16 @@ using namespace std;
 		cerr << "code : "<< error << " reason : "<<cudaGetErrorString(error) << endl;	\
 	}														\
 }
-
-
+/*
 void checkResult(float* hostRef,float* devRef,const int N){
-  float epsilon = 1e-3;
+  float epsilon = 1.0E-4;
   bool match = 1;
   int i;
-  float Ref;
-  float Host;
-  float Dev;
   for(i=0;i<N;i++){
-  	Host = hostRef[i];
-  	Dev = devRef[i];
-  	Ref = Host-Dev;
-
     //printf("host:%d,device:%d\n",hostRef[i],devRef[i]);
-    if((float)fabsf(Ref)>epsilon){
+    if(abs(hostRef[i]-devRef[i])>epsilon){
       match = 0;
-      cout << "Arrays don't match.on count of "<<i<< " element." <<endl;
-      cout << "Elapsed : " << Ref << " Host : " << Host << " | GPU : " << Dev << endl; 
-      printf("Elapsed : %f Host : %f GPU : %f\n",Ref,Host,Dev );
+      cout << "Arrays don't match.on count of "<<i<< " element.";
       break;
     }
   }
@@ -51,6 +40,7 @@ void checkResult(float* hostRef,float* devRef,const int N){
   cout << endl;
   return;
 }
+*/
 
 void initializeData(float* A,int size){
   //乱数で値を初期化します。
@@ -59,7 +49,7 @@ void initializeData(float* A,int size){
   srand((unsigned int)time(&t));
 
   for(i=0;i<size;i++){
-    A[i] = (float)(rand()*0xFFF) / 100000.0F;
+    A[i] = (float)(rand()) / 10.0F;
   }
   return;
 }
@@ -75,6 +65,17 @@ void print(float* Def,float* Src,float* Rst,const int elem){
 		cout << "\t" <<i << " | " << Def[i] << " | " <<Src[i] << " | "<<Rst[i] << endl;
 	}
 }
+/*
+void Host1DStencil(float* Src,float* Dst){
+	for(int st=0;st<STEP;st++){
+		for(int i=0;i<ELEM;i++){
+			if(i!=0 && i<ELEM-1)
+				Dst[i] = 0.6*Src[i] + 0.2*(Src[i-1]+Src[i+1]);
+		}
+		swap(Src,Dst);
+	}
+}
+*/
 
 void Host2DStencil(float* Src,float* Dst){
 	for(int time_step=0;time_step<STEP;time_step++){
@@ -86,32 +87,26 @@ void Host2DStencil(float* Src,float* Dst){
 				//端ならば計算しない
 				Dst[all_loop] = 0.6*Src[all_loop] + 0.1*(Src[all_loop-1]+Src[all_loop+1]+Src[all_loop-X]+Src[all_loop+X]);
 			}
+			cout << endl;
 		}
 		swap(Src,Dst);
 	}
 }
-//Single版
-__global__ void StencilOneStep(float* Src,float* Dst){
-	size_t  index = threadIdx.x + blockDim.x * blockIdx.x;
-	size_t mat_x = index % X; //X成分
-	size_t mat_y = index / X; //Y成分
-	if(mat_x != 0 && mat_x != X-1 && mat_y != 0 && mat_y != Y-1){
-		Dst[index] = 0.6*Src[index] + 0.1*(Src[index-1] + Src[index+1] + Src[index+X] + Src[index-X]);
-	}
-} 
 
 int main(int argc,char** argv){
 	float* Src = new float[ELEM];
 	float* Dst = new float[ELEM];
-	float* Rst = new float[ELEM];
-	float* Def = new float[ELEM];
+
+//	float* Rst = new float[ELEM];
+//	float* Def = new float[ELEM];
 	//Srcを乱数で初期化
 	initializeData(Src,ELEM);
 
 	memcpy(Dst,Src,sizeof(float)*ELEM);
-	memcpy(Def,Src,sizeof(float)*ELEM);
+
+//	memcpy(Def,Src,sizeof(float)*ELEM);
 	//Deviceメモリの確保
-	
+	/*
 	size_t DeviceMemorySize = ELEM*sizeof(float);
 	float *d_Src,*d_Dst;
 	CHECK(cudaSetDevice(0));
@@ -127,7 +122,7 @@ int main(int argc,char** argv){
 	cout << "block : "<< block.x << " | grid : " << grid.x << endl;
 
 	for(int st=0;st<STEP;st++){
-		StencilOneStep<<<grid,block>>>(d_Src,d_Dst);
+		StencilOneStep<<<grid,block>>>(d_Src,d_Dst,ELEM);
 		swap(d_Src,d_Dst);
 	}
 	CHECK(cudaGetLastError());
@@ -135,14 +130,15 @@ int main(int argc,char** argv){
 	CHECK(cudaMemcpy(Rst,d_Src,DeviceMemorySize,cudaMemcpyDeviceToHost));
 	CHECK(cudaFree(d_Src));
 	CHECK(cudaFree(d_Dst));
+*/
 	Host2DStencil(Src,Dst);
 //	print(Src);
-	checkResult(Src,Rst,ELEM);
+//	checkResult(Src,Rst,ELEM);
 
 //	print(Def,Src,Rst,ELEM);
 	delete Src;
 	delete Dst;
-	delete Rst;
-	delete Def;
+//	delete Rst;
+//	delete Def;
 	return 0;
 }
